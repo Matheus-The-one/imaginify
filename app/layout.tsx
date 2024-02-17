f97c25682d857b6c340e48ce4335c2028015611b
Mate papava <matepapava123@gmail.com>
2024-02-17 23:39:17 +0400

    adding auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBMplex= IBM_Plex_Sans({
   subsets:['latin'],
@@ -23,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={cn("font-IBMplex antialiased",IBMplex.variable)}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
